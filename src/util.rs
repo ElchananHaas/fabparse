c59feb2b7d313f980e6023638d7dc41e3cb705d3
Elchanan Haas <haas2376@gmail.com>
2024-01-14 15:27:47 -0800

    Added a util module

--- src/util.rs
+++ src/util.rs
@@ -1,21 +1,51 @@
 use crate::{opt, sequence::Sequence, Parser, ParserError};
 
-pub fn num_unsigned<'a, E: ParserError>(input: &mut &'a str) -> Result<&'a str, E>
+/**
+ * This string can be parsed as an unsigned number
+ */
+pub fn num_unsigned_str<'a, E: ParserError>(input: &mut &'a str) -> Result<&'a str, E>
 where
 {
     char::is_ascii_digit
         .fab_repeat()
+        .min(1)
         .as_input_slice()
         .fab(input)
 }
-pub fn num_signed<'a, E: ParserError>(input: &mut &'a str) -> Result<&'a str, E>
+/**
+ * This string can be parsed as a signed number
+ */
+pub fn num_signed_str<'a, E: ParserError>(input: &mut &'a str) -> Result<&'a str, E>
 where
 {
     let orig_input = *input;
     let _sign = opt('-').fab(input)?;
-    let _rest = (|c: char| c.is_ascii_digit())
+    let _rest = char::is_ascii_digit
         .fab_repeat()
+        .min(1)
         .as_input_slice()
         .fab(input)?;
+    Ok(orig_input.subtract(*input))
+}
+/**
+ * This string can be parsed as a float or double
+ */
+pub fn float_str<'a, E: ParserError>(input: &mut &'a str) -> Result<&'a str, E>
+where
+{
+    let orig_input = *input;
+    let _sign = opt('-').fab(input)?;
+    let _whole_part = char::is_ascii_digit
+        .fab_repeat()
+        .min(1)
+        .as_input_slice()
+        .fab(input)?;
+    let _decimal_part = opt((
+        '.',
+        char::is_ascii_digit
+        .fab_repeat()
+        .min(1)
+        .as_input_slice()
+    )).fab(input)?;
     Ok(orig_input.subtract(*input))
 }
